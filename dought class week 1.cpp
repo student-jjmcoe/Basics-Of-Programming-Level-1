033f4285092ca7b801c48b66060f1a5b218600c7
student-jjmcoe <150603427+student-jjmcoe@users.noreply.github.com>
2023-11-23 15:18:24 +0530

    Add files via upload

---
+++ dought class week 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//using namespace std;
+#define AUTHOR "shweta";//macrose
+int main(){
+    int a;
+    std::cin>>a;
+    a=a*a;
+    std::cout<<"square of a : "<<a<<std::endl;
+    std::cout<<"written by : "<< AUTHOR ;
+    std::cout<<std::endl;
+    std::cout<<"written by : "<< AUTHOR ;
+    std::cout<<std::endl;
+    return 0;
+}
